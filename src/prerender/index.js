34c336b8a44a804cce945fddfbe1ac0967d01b0c
manuelJung <manuel.jung@emmos.de>
2018-06-01 16:15:52 +0200

    renamed cli script

--- src/prerender/index.js
+++ src/prerender/index.js
@@ -5,11 +5,11 @@ import Loadable from 'react-loadable'
 import LoadableCapture from './LoadableCapture'
 import { addMiddleware } from 'redux-dynamic-middlewares'
 import { ServerStyleSheet, __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS } from 'styled-components'
-import {IS_SERVER} from './const'
+import {IS_CRAWLER} from './const'
  
 
 export const render = (RootComponent, domElement, store) => {
-  if (IS_SERVER) {
+  if (IS_CRAWLER) {
     window.reactSnapshotRender(
       Loadable.preloadAll()
       .then(() => collectStoreData(RootComponent, store, domElement))
